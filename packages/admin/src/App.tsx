1c3e80da31e399801478f61d268be158e79ce633
Pretty Boy Deinla <prettyboydeinla11@gmail.com>
2023-03-16 15:02:55 +0400

    feat: tailwind config adjustments; button styles;

--- packages/admin/src/App.tsx
+++ packages/admin/src/App.tsx
@@ -1,42 +1,40 @@
-import React, { useState } from "react";
+import React, {useState} from 'react';
 
-import "./App.css";
-import "@fastraxglobal/shared/lib/assets/global.css";
-import { Button } from "@fastraxglobal/shared";
-
-// const { Coding, Layout } = DuoTone;
-
-// const { Cod001 } = Coding;
-// const { Lay001 } = Layout;
+import './App.css';
+import '@fastraxglobal/shared/lib/assets/global.css';
+import {Button} from '@fastraxglobal/shared';
 
 function App() {
-      const userTheme = localStorage.getItem("theme");
-      const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
-            .matches;
+  const userTheme = localStorage.getItem('theme');
+  const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
-      const [theme, setTheme] = useState("Dark");
+  const [theme, setTheme] = useState('Dark');
 
-      const themeCheck = React.useCallback(() => {
-            if (userTheme === "dark" || (!userTheme && systemTheme)) {
-                  document.documentElement.classList.add("dark");
-            }
-      }, [userTheme, systemTheme]);
+  const themeCheck = React.useCallback(() => {
+    if (userTheme === 'dark' || (!userTheme && systemTheme)) {
+      document.documentElement.classList.add('dark');
+    }
+  }, [userTheme, systemTheme]);
 
-      const themeSwitch = React.useCallback(() => {
-            if (document.documentElement.classList.contains("dark")) {
-                  document.documentElement.classList.remove("dark");
-                  localStorage.setItem("theme", "light");
-            } else {
-                  document.documentElement.classList.add("dark");
-                  localStorage.setItem("theme", "dark");
-            }
-      }, [userTheme, systemTheme]);
+  const themeSwitch = React.useCallback(() => {
+    if (document.documentElement.classList.contains('dark')) {
+      document.documentElement.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
+    } else {
+      document.documentElement.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+    }
+  }, [userTheme, systemTheme]);
 
-      React.useEffect(() => {
-            themeCheck();
-      }, []);
+  React.useEffect(() => {
+    themeCheck();
+  }, []);
 
-      return <div id="app" className="dark:bg-slate-900"></div>;
+  return (
+    <div id="app" className="dark:bg-slate-900">
+      <Button>Go To Home Page</Button>
+    </div>
+  );
 }
 
 export default App;
--- packages/admin/tailwind.config.js
+++ packages/admin/tailwind.config.js
@@ -1,10 +1,10 @@
-const config = require("@fastraxglobal/shared/tailwind.config");
+const config = require('@fastraxglobal/shared/tailwind.config');
 
 module.exports = {
-      ...config,
-      content: [
-            "./public/**/*.html",
-            "./src/**/*.{js,jsx,ts,tsx,vue}",
-            "./lib/**/*.{js,jsx,ts,tsx,vue}",
-      ],
+  ...config,
+  content: [
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+    './lib/**/*.{js,jsx,ts,tsx,vue}',
+  ],
 };
